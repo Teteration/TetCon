2d5461fadbaadfae6f8119eccef7c42f31691f9d
mahdipro <mahdihamedi79@gmail.com>
2021-12-28 01:20:41 +0330

    unstable version : some user friendly

--- Tetcon.sh
+++ Tetcon.sh
@@ -1,10 +1,105 @@
 #!/bin/bash
 
+
+
 figlet Tetcon | lolcat
-echo "Try 'Tetcon -h' for more information."
+
+a=$'\n\n\n'
+echo "$a"
+
+
+# COLOR
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+if [ -z $1 ] && [ -z $2 ] && [ -z $3 ];
+then
+    echo -e "${Red}[*]${Color_Off} Try 'Tetcon -h' for more information."
+fi
 
 
 
@@ -27,17 +122,20 @@ portScanner()
 
 crawl(){
     bash ./Functions/crawl.sh $domain $blacklist
+    exit
 }
 
 subDomain()
 {
     bash ./Functions/subDomain.sh $domain
+    exit
 }
 
 
 fuzzer()
 {
     bash ./Functions/fuzzer.sh $domain $wordlist
+    exit
 }
 
 
@@ -50,28 +148,53 @@ case "$1" in
 
 
     -p|--portScan)
-        ip=$2
-        portScanner
+        if [ -z $2 ];
+        then
+            echo "enter url"
+        else 
+            ip=$2
+            portScanner
+        fi
         ;;
 
 
     -c|--crawl)
-        domain=$2
-        blacklist=$4
-        crawl
+        if [ -z $2 ];
+        then
+            echo "Missing Domain"
+        else 
+            domain=$2
+            blacklist=$4
+            crawl
+            
+        fi    
         ;;
 
 
     -sd|--subDomainDiscovery)
-        domain=$2
-        subDomain
+        if [ -z $2 ];
+        then
+            echo "domain not provide"
+        else 
+            domain=$2
+            subDomain
+
+        fi
         ;;
 
 #Tetcon -f url -w wordlist
     -f|--fuzzer)
-        domain=$2
-        wordlist=$4
-        fuzzer
+        if [ -z $2 ];
+        then
+            echo "Fuzzer : Missing domain"
+        elif [ -z $4 ] && [ $2 ];
+        then
+            echo "Fuzzer : Missing word list"
+        else 
+            domain=$2
+            wordlist=$4
+            fuzzer
+        fi  
         ;;
 
 
